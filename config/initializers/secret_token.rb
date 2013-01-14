b19fdb03c61a158c264a1da957bbb2bcbbd5b2ff
Phil Muir <phil.muir@bigcommerce.com>
2013-01-14 15:23:42 +1100

    Updated secret_token and cookie key

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-PowerdnsOnRails::Application.config.secret_token = 'af1fc9bcdf8b08cf13215f84b0a6c4429c5fcbcbe7eae952d26efdf5190f09b80db936fbf5e2bef35c4395abee9ca819d5d6ee705f86f841afc2559525bb4e84'
+PowerdnsOnRails::Application.config.secret_token = '0b19c4b5f85ef74bb208af09f00eec1f0d44346db6ed744580e6e6baf40482df974e99ae70fc9cc0a7d8e955b34bfef99deb7679aadf061524a384739853a252'
